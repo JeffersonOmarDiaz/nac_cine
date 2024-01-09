5247a5e5e4e34ae3b6441cee51cacb7d2d3ccfdf
JeffersonOmarDiaz <jefferson.diaz@epn.edu.ec>
2024-01-09 11:53:00 -0500

    Entidades, taquilla, peliculaGenero, pelicula, genero

---
+++ nac_cine/cine_acceso_datos/Entidades/PeliculaGenero.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cine_acceso_datos.Entidades
+{
+    internal class PeliculaGenero
+    {
+        private int idPeliculaGenero {  get; set; }
+        private int idPelicula { get; set; }
+        private int idGenero { get; set; }
+        private int estado { get; set; }
+
+    }
+}
