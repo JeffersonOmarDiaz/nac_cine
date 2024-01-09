78fd96464493a283af78895fcfba351917b3233e
JeffersonOmarDiaz <jefferson.diaz@epn.edu.ec>
2024-01-09 11:37:15 -0500

    Asiento y cine

---
+++ nac_cine/cine_acceso_datos/Entidades/Asiento.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cine_acceso_datos.Entidades
+{
+    internal class Asiento
+    {
+        private int idAsiento {  get; set; }
+        private int idSala { get; set; }
+        private string numeroAsiento { get; set; }
+        private string filaAsiento { get; set; }
+        private int ocupado { get; set; }
+        private int estado { get; set; }
+    }
+}
---
+++ nac_cine/cine_acceso_datos/Entidades/Cine.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cine_acceso_datos.Entidades
+{
+    internal class Cine
+    {
+        private int idCine { get; set; }
+        private string nombre { get; set; }
+        private string direccion { get; set; }
+        private string horario_apertura { get; set; }
+        private string horario_cierre { get; set; }
+        private int estado { get; set; }
+    }
+}
