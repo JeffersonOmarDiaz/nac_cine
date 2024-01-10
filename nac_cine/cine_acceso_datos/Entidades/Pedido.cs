d28b50f5ec2d5cf16fc18fc1aa05f636d6b71b17
Carlos <e1750359612@uisrael.edu.ec>
2024-01-09 21:55:54 -0500

    entidad Pedido

---
+++ nac_cine/cine_acceso_datos/Entidades/Pedido.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cine_acceso_datos.Entidades
+{
+    public class Pedido
+    {
+        private int idPedido { get; set; }
+        private int idPelicula { get; set; }
+        private int idProveedor { get; set; }
+        private string fechaPedido { get; set; }
+        private int EstadoPedido { get; set; }
+        private int PedidoValor { get; set; }
+
+        private int Estado { get; set; }
+    }
+}
+
