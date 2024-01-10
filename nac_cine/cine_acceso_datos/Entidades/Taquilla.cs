0b52e0c8f333ea97a99a57c81a5bcc9588805c64
JeffersonOmarDiaz <jefferson.diaz@epn.edu.ec>
2024-01-10 09:14:08 -0500

    Entidades restantes

---
+++ nac_cine/cine_acceso_datos/Entidades/Taquilla.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cine_acceso_datos.Entidades
+{
+    internal class Taquilla
+    {
+        private int idTaquilla {  get; set; }
+        private int idCine { get; set; }
+        private int idUsuario { get; set; }
+        private int idNumeroTaquilla { get; set; }
+        private int estado { get; set; }
+
+    }
+}
