25892339a32088d1d4fda19a2c3e2db0db18cbb2
Carlos <e1750359612@uisrael.edu.ec>
2024-01-09 22:36:39 -0500

    Entidad Proveedor

---
+++ nac_cine/cine_acceso_datos/Entidades/Proveedor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cine_acceso_datos.Entidades
+{
+    public class Proveedor
+    {
+        private int idProveedor { get; set; }
+        private string Nombre { get; set; }
+        private string direccion { get; set; }
+        private string email { get; set; }
+        private string ruc { get; set; }
+        private int estado { get; set; }
+
+    }
+}
