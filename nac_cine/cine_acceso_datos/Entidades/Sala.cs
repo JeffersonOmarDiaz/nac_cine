aabad2e21a277e23d164c55cf1c1250415ea7991
Chelo1988 <marcelothuglife@hotmail.com>
2024-01-10 01:27:50 -0500

    Clase Sala Add

---
+++ nac_cine/cine_acceso_datos/Entidades/Sala.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cine_acceso_datos.Entidades
+{
+    public class Sala
+    {
+        private int idSala { get; set; }
+        private int idCine { get; set; }
+        private string nombreSala { get; set; }
+        private string estadoSala { get; set; }
+    }
+}
