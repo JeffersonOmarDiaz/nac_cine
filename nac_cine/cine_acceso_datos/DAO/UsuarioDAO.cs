ec1a49c879728a3e2f8d931cae44ea3fbbb5b719
Chelo1988 <marcelothuglife@hotmail.com>
2024-01-21 21:28:20 -0500

    DAO Y LOGICA USUARIO

---
+++ nac_cine/cine_acceso_datos/DAO/UsuarioDAO.cs
@@ -0,0 +1,149 @@
+﻿using cine_acceso_datos.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cine_acceso_datos.DAO
+{
+    public class UsuarioDAO
+    {
+        private ConexionBD conexionDB = new ConexionBD();
+        SqlCommand ejecutarSql = new SqlCommand();
+        SqlDataReader transaccion;
+
+        public void InsertarUsuario(Usuario nuevoUsuario)
+        {
+
+            try
+            {
+                ejecutarSql.Connection = conexionDB.AbrirConexion();
+                ejecutarSql.CommandText = "insert into USUARIO (NOMBRE_USUARIO,APELLIDO_USUARIO,CEDULA_USUARIO,EMAIL_USUARIO,TELEFONO_USUARIO,EDAD,USERNAME,PASSWORD,ESTADO)" +
+                                          "values('"
+                                          + nuevoUsuario.nombreUsuario + "','"
+                                          + nuevoUsuario.apellidoUsuario + "','"
+                                          + nuevoUsuario.cedulaUsuario + "','"
+                                          + nuevoUsuario.emailUsuario + "','"
+                                          + nuevoUsuario.telefonoUsuario + "',"
+                                          + nuevoUsuario.edadUsuario + ",'"
+                                          + nuevoUsuario.usernameUsuario + "','"
+                                          + nuevoUsuario.passwordUsuario + "',"
+                                          + nuevoUsuario.estadoUsuario
+                                          + ")";
+                ejecutarSql.ExecuteNonQuery();
+                conexionDB.CerrarConexion();
+
+            }
+            catch (Exception error)
+            {
+
+                throw new Exception("Error no se puede insertar el usuario " + error.Message);
+            }
+
+        }
+
+        public DataTable ListarUsuario()
+        {
+
+            try
+            {
+                DataTable dataTableUsuario = new DataTable();
+                ejecutarSql.Connection = conexionDB.AbrirConexion();
+                ejecutarSql.CommandText = "Select * from USUARIO";
+                transaccion = ejecutarSql.ExecuteReader();
+                dataTableUsuario.Load(transaccion);
+                conexionDB.CerrarConexion();
+                return dataTableUsuario;
+
+
+
+            }
+            catch (Exception error)
+            {
+
+                throw new Exception("Error no se puede listar los usuarios " + error.Message);
+            }
+
+        }
+
+        public DataTable BuscarUsuarioCI(string CedulaUsuario)
+        {
+
+            try
+            {
+                DataTable dataTableUsuario = new DataTable();
+                ejecutarSql.Connection = conexionDB.AbrirConexion();
+                ejecutarSql.CommandText = "Select * from USUARIO where CEDULA_USUARIO='"+ CedulaUsuario + "'";
+                transaccion = ejecutarSql.ExecuteReader();
+                dataTableUsuario.Load(transaccion);
+                conexionDB.CerrarConexion();
+                return dataTableUsuario;
+
+
+
+            }
+            catch (Exception error)
+            {
+
+                throw new Exception("Error al listar el usuario " + error.Message);
+            }
+
+        }
+
+        public void ActualizarUsuario(Usuario nuevoUsuario)
+        {
+
+            try
+            {
+                ejecutarSql.Connection = conexionDB.AbrirConexion();
+                ejecutarSql.CommandText = "update USUARIO set "
+                                          + "NOMBRE_USUARIO ='" + nuevoUsuario.nombreUsuario + "','"
+                                          + "APELLIDO_USUARIO ='" + nuevoUsuario.apellidoUsuario + "','"
+                                          + "CEDULA_USUARIO ='" + nuevoUsuario.cedulaUsuario + "','"
+                                          + "EMAIL_USUARIO ='" + nuevoUsuario.emailUsuario + "','"
+                                          + "TELEFONO_USUARIO ='" + nuevoUsuario.telefonoUsuario + "',"
+                                          + "EDAD ='" + nuevoUsuario.edadUsuario + ",'"
+                                          + "USERNAME ='" + nuevoUsuario.usernameUsuario + "','"
+                                          + "PASSWORD ='" + nuevoUsuario.passwordUsuario + "',"
+                                          + "ESTADO ='" + nuevoUsuario.estadoUsuario +
+                                          " where ID_USUARIO = "+nuevoUsuario.idUsuario+" "; 
+                ejecutarSql.ExecuteNonQuery();
+                conexionDB.CerrarConexion();
+
+            }
+            catch (Exception error)
+            {
+
+                throw new Exception("Error no se puede actualizar el usuario " + error.Message);
+            }
+
+        }
+
+        public void EliminarUsuario(int idUsuario)
+        {
+
+            try
+            {
+                
+                ejecutarSql.Connection = conexionDB.AbrirConexion();
+                ejecutarSql.CommandText = "Delete from USUARIO where ID_USUARIO=" + idUsuario + " ";
+                ejecutarSql.ExecuteNonQuery();
+                conexionDB.CerrarConexion();
+  
+
+
+
+            }
+            catch (Exception error)
+            {
+
+                throw new Exception("Error al eliminar el usuario " + error.Message);
+            }
+
+        }
+
+    }
+}
---
+++ nac_cine/cine_logica_negocios/UsuarioLogica.cs
@@ -0,0 +1,104 @@
+﻿using cine_acceso_datos;
+using cine_acceso_datos.DAO;
+using cine_acceso_datos.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cine_logica_negocios
+{
+    public class UsuarioLogica
+    {
+        private UsuarioDAO usuarioDAO= new UsuarioDAO();
+
+        public Boolean insertarUsuario(Usuario nuevoUsuario)
+        {
+            try
+            {
+                usuarioDAO.InsertarUsuario(nuevoUsuario);
+                return true;
+
+            }
+            catch (Exception error)
+            {
+
+                throw new Exception("Error al insertar el usuario " + error.Message);
+            }
+           
+
+        }
+
+
+        public DataTable ListarUsuario()
+        {
+            try
+            {
+                return usuarioDAO.ListarUsuario();
+
+            }
+            catch (Exception error)
+            {
+
+                throw new Exception("Error al listar los usuarios " + error.Message);
+            }
+
+
+        }
+
+        public DataTable BuscarUsuario(string CedulaUsuario)
+        {
+            try
+            {
+                return usuarioDAO.BuscarUsuarioCI(CedulaUsuario);
+
+            }
+            catch (Exception error)
+            {
+
+                throw new Exception("Error al listar el usuario por CI " + error.Message);
+            }
+
+
+        }
+
+        public Boolean ActualizarUsuario(Usuario nuevoUsuario)
+        {
+            try
+            {
+                usuarioDAO.ActualizarUsuario(nuevoUsuario);
+                return true;
+
+            }
+            catch (Exception error)
+            {
+
+                throw new Exception("Error al listar el usuario por CI " + error.Message);
+            }
+
+
+        }
+
+        public Boolean EliminarUsuario(int idUsuario)
+        {
+            try
+            {
+                usuarioDAO.EliminarUsuario(idUsuario);
+                return true;
+
+            }
+            catch (Exception error)
+            {
+
+                throw new Exception("Error al eliminar el usuario " + error.Message);
+            }
+
+
+        }
+
+
+
+    }
+}
