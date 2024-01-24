83052c07fdbc64e4ce4ddbc728917283769cb781
CarlosLema26 <e1750359612@uisrael.edu.ec>
2024-01-24 17:26:20 -0500

    Se añadio Pedido,Programacion,Rol y Proveedor DAO

---
+++ nac_cine/cine_acceso_datos/DAO/ProgramacionDAO.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cine_acceso_datos.DAO
+{
+    public class ProgramacionDAO
+    {
+
+        private ConexionDB conexion = new ConexionDB();
+        SqlDataReader leer;
+        DataTable tabla = new DataTable();
+        SqlCommand comando = new SqlCommand();
+
+        public DataTable Mostrar()
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "Mostrar Programacion";
+            comando.CommandType = CommandType.StoredProcedure;
+            leer = comando.ExecuteReader();
+            tabla.Load(leer);
+            conexion.AbrirConexion();
+            return tabla;
+
+        }
+        public void InsertarProgramacion(Programacion nuevo)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "Insertar Programacion";
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@idProgramacion ", nuevo.idProgramacion);
+            comando.Parameters.AddWithValue("@idSala", nuevo.idSala);
+            comando.Parameters.AddWithValue("@idPelicula", nuevo.idPelicula);
+            comando.Parameters.AddWithValue("@idUsuario", nuevo.idUsuario);
+            comando.Parameters.AddWithValue("@Horainicio", nuevo.HoraInicio);
+            comando.Parameters.AddWithValue("@HoraFin", nuevo.HoraFin);
+            comando.Parameters.AddWithValue("@estreno", nuevo.estreno);
+            comando.Parameters.AddWithValue("@estado", nuevo.estado);
+            comando.ExecuteNonQuery();
+            comando.Parameters.Clear();
+            conexion.CerrarConexion();
+
+        }
+
+    }
+}
---
+++ nac_cine/cine_acceso_datos/DAO/RolDao.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cine_acceso_datos.DAO
+{
+    public class RolDao
+
+    {
+        private ConexionDB conexion = new ConexionDB();
+        SqlDataReader leer;
+        DataTable tabla = new DataTable();
+        SqlCommand comando = new SqlCommand();
+
+        public DataTable Mostrar()
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "Mostrar Rol";
+            comando.CommandType = CommandType.StoredProcedure;
+            leer = comando.ExecuteReader();
+            tabla.Load(leer);
+            conexion.AbrirConexion();
+            return tabla;
+
+        }
+        public void InsertarRol(Rol nuevo)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "Insertar Rol";
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@idRole", nuevo.idRole);
+            comando.Parameters.AddWithValue("@idUsuario", nuevo.idUsuario);
+            comando.Parameters.AddWithValue("@Nombre", nuevo.Nombre);
+            comando.Parameters.AddWithValue("@Estado", nuevo.Estado);
+            comando.ExecuteNonQuery();
+            comando.Parameters.Clear();
+            conexion.CerrarConexion();
+
+        }
+
+    }
+}
