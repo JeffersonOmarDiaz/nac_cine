9999cb4cb8401b809965a3950b708c0f190c1308
CarlosLema26 <e1750359612@uisrael.edu.ec>
2024-01-24 21:08:50 -0500

    LOGICAS NUEVAS

---
+++ nac_cine/cine_logica_negocios/ProveedorLogica.cs
@@ -0,0 +1,30 @@
+﻿using cine_acceso_datos.DAO;
+using cine_acceso_datos.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cine_logica_negocios
+{
+    public class ProveedorLogica
+    {
+        private ProveedorDAO proveedorDAO = new ProveedorDAO();
+        public Boolean InsertarProveedor(Proveedor nuevoProveedor)
+        {
+            proveedorDAO.InsertarProveedor(nuevoProveedor);
+            return true;
+
+
+        }
+        public DataTable Mostrar()
+        {
+            return proveedorDAO.Mostrar();
+        }
+
+
+
+    }
+}
