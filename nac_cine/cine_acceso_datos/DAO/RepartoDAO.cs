446a111a9b7b731d3428d778c6c289c769277abe
CarlosLema26 <e1750359612@uisrael.edu.ec>
2024-01-25 20:38:24 -0500

    agregar Reparto

---
+++ nac_cine/cine_acceso_datos/DAO/RepartoDAO.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cine_acceso_datos.DAO
+{
+    public class RepartoDAO
+    {
+
+        private ConexionDB conexion = new ConexionDB();
+        SqlDataReader leer;
+        DataTable tabla = new DataTable();
+        SqlCommand comando = new SqlCommand();
+
+        public DataTable Mostrar()
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "Mostrar Reparto";
+            comando.CommandType = CommandType.StoredProcedure;
+            leer = comando.ExecuteReader();
+            tabla.Load(leer);
+            conexion.AbrirConexion();
+            return tabla;
+
+        }
+        public void InsertarReparto(Reparto nuevo)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "Insertar Reparto";
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@idReparto ", nuevo.idReparto);
+            comando.Parameters.AddWithValue("@idPelicula", nuevo.idPelicula);
+            comando.Parameters.AddWithValue("@idNombreActor", nuevo.idNombreActor);
+            comando.Parameters.AddWithValue("@idPersonaje", nuevo.idEstado);         
+            comando.Parameters.AddWithValue("@estado", nuevo.estado);
+            comando.ExecuteNonQuery();
+            comando.Parameters.Clear();
+            conexion.CerrarConexion();
+
+        }
+
+    }
+}
---
+++ nac_cine/cine_logica_negocios/RepartoLogica.cs
@@ -0,0 +1,29 @@
+﻿using cine_acceso_datos.DAO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cine_logica_negocios
+{
+    public class RepartoLogica
+    {
+        private RepartoDAO repartoDao = new RepartoDAO();
+        public Boolean InsertarReparto(Reparto nuevoReparto)
+        {
+            RepartoDAO.InsertarReparto(nuevoReparto);
+            return true;
+
+
+        }
+        public DataTable Mostrar()
+        {
+            return repartoDao.Mostrar();
+        }
+
+
+    
+    }
+}
